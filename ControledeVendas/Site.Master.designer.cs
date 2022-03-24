1ce398fa12076aa3e0d4700e3a4a5e421539f133
cinthiabs <cinthiabarbosa8d@outlook.com>
2022-03-24 08:56:24 -0300

    Sistema Web

---
+++ ControledeVendas/Site.Master.designer.cs
@@ -0,0 +1,35 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace ControledeVendas
+{
+
+
+    public partial class SiteMaster
+    {
+
+        /// <summary>
+        /// Controle header.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlHead header;
+
+        /// <summary>
+        /// Controle MainContent.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+    }
+}
